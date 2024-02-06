18f3e233b875e2168ea22a75e4194078c84fa60e
BreadUniverse <marianaroan70@gmail.com>
2024-02-06 21:49:07 +0300

    Вот такие пироги

--- app/src/main/java/com/maria/deliveryclub/Db/DbHelper.kt
+++ app/src/main/java/com/maria/deliveryclub/Db/DbHelper.kt
@@ -6,15 +6,21 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
-    SQLiteOpenHelper(context, "app", factory, 1) {
+    SQLiteOpenHelper(context, "app", factory, 2) {
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val query = "CREATE TABLE users (id INT PRIMARY KEY, login TEXT, email TEXT, pass TEXT)"
+        val query = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, login TEXT, email TEXT, pass TEXT)"
+        val query1 = "CREATE TABLE IF NOT EXISTS couriers (id INT PRIMARY KEY, login TEXT, email TEXT, pass TEXT)"
+        val query2 = "CREATE TABLE IF NOT EXISTS dispatchers (id INT PRIMARY KEY, login TEXT, email TEXT, pass TEXT)"
         db!!.execSQL(query)
+        db!!.execSQL(query1)
+        db!!.execSQL(query2)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS users")
+        db!!.execSQL("DROP TABLE IF EXISTS couriers")
+        db!!.execSQL("DROP TABLE IF EXISTS dispatchers")
         onCreate(db)
     }
 
@@ -44,7 +50,7 @@ class DbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         values.put("pass", courier.pass)
 
         val db = this.writableDatabase
-        db.insert("users", null, values)
+        db.insert("couriers", null, values)
 
         db.close()
     }
@@ -52,7 +58,7 @@ class DbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     fun getCourier(login: String, pass: String): Boolean {
         val db = this.readableDatabase
 
-        val result = db.rawQuery("SELECT * FROM users  WHERE login = '$login' AND pass = '$pass'", null)
+        val result = db.rawQuery("SELECT * FROM couriers  WHERE login = '$login' AND pass = '$pass'", null)
         return result.moveToFirst()
     }
 
@@ -63,7 +69,7 @@ class DbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         values.put("pass", dispatcher.pass)
 
         val db = this.writableDatabase
-        db.insert("users", null, values)
+        db.insert("dispatchers", null, values)
 
         db.close()
     }
@@ -71,7 +77,7 @@ class DbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     fun getDispatcher(login: String, pass: String): Boolean {
         val db = this.readableDatabase
 
-        val result = db.rawQuery("SELECT * FROM users  WHERE login = '$login' AND pass = '$pass'", null)
+        val result = db.rawQuery("SELECT * FROM dispatchers  WHERE login = '$login' AND pass = '$pass'", null)
         return result.moveToFirst()
     }
 
